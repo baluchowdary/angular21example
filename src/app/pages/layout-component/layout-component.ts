13a54f0af4f3653f07e2c14751311276191a64c4
baluchowdary <bhardwaja.kollu@gmail.com>
2026-01-30 09:39:02 -0600

    Added logout feature from dashboard page

--- src/app/pages/layout-component/layout-component.ts
+++ src/app/pages/layout-component/layout-component.ts
@@ -3,7 +3,7 @@ import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout-component',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet],
   templateUrl: './layout-component.html',
   styleUrls: ['./layout-component.css'],
 })
@@ -13,9 +13,17 @@ export class LayoutComponent {
 //router = inject(Router);
 
 
-  redirectLogin() {
+  // redirectLogin() {
+  //   debugger;
+  //   console.log('Redirecting to login page');
+  //   this.router.navigateByUrl('/login');
+  //   //window.location.href = '/login';
+  // }
+
+  logout() {
     debugger;
-    console.log('Redirecting to login page');
+    console.log('Logging out and redirecting to login page');
+    localStorage.removeItem('authdata');
     this.router.navigateByUrl('/login');
     //window.location.href = '/login';
   }
